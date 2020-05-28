f4eaeb567940f6cc945ea51e18494b04018d9e5c
Ricardo Guevara Aragón <RickAr2@hotmail.com>
2020-05-28 14:23:02 -0600

    Express escucha en localhost:5000 y sirve el fichero index.html ante peticiones. Express sirve ficheros estaticos de recursos para la pagina html. Renombrado el fichero 'principal.js' por 'api.js'y agregadas las importaciones iniciales.

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const api = require('./servidor/api');
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.static(path.join(__dirname, 'www')));
+
+app.get('/', function(req, res, next){
+    res.sendFile(path.resolve(__dirname, 'www/index.html'), function(errBack){
+        if(errBack) console.error(errBack.message);
+        res.end();
+    });
+});
+
+app.use('/api', api);
+
+app.listen(PORT, function(){
+    console.log(`Escuchando en localhost:${PORT}`);
+});
