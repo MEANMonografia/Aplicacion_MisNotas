b09a0d0be4b8a6e7e0f5b8f83ded204100ab7cde
Ricardo Guevara Aragón <RickAr2@hotmail.com>
2020-06-05 10:29:05 -0600

    Renombrados los endpoints CRUD

--- servidor/CrudRouter.js
+++ servidor/CrudRouter.js
@@ -1,26 +1,22 @@
 const router = require('express').Router();
 
-router.post('/crearnota', (req, res, next)=>{
+router.post('/crear', (req, res, next)=>{
 
 });
 
-router.post('/modificarnota', (req, res, next)=>{
+router.post('/modificar', (req, res, next)=>{
 
 });
 
-router.post('/eliminarnota', (req, res, next)=>{
+router.post('/eliminar', (req, res, next)=>{
 
 });
 
-router.post('/buscarnota', (req, res, next)=>{
+router.post('/getnotas', (req, res, next)=>{
 
 });
 
 router.post('/setnotasfijas', (req, res, next)=>{
-
-});
-
-router.post('/getnotasfijas', (req, res, next)=>{
 
 });
 
